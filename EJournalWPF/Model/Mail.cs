00a0133159e8c2b9df03f50c0b1f3c8a661ad377
Net2Fox <trakounet.fox@gmail.com>
2024-10-04 14:20:22 +0300

    Классы файла, почты и статуса

---
+++ EJournalWPF/Model/Mail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJournalWPF.Model
+{
+    internal class Mail
+    {
+        public long ID { get; set; }
+        public DateTime Date { get; set; }
+        public string Subject { get; set; }
+        public Student FromUser { get; set; }
+        public Status Status { get; set; }
+        public List<File> Files { get; set; }
+        public bool hasFiles { get; set; }
+
+    }
+}
