d50ef619aafc7b2b2c9f8f957669e9af36d39418
Net2Fox <trakounet.fox@gmail.com>
2024-10-04 14:25:47 +0300

    Изменено поле статуса

--- EJournalWPF/Model/Mail.cs
+++ EJournalWPF/Model/Mail.cs
@@ -12,7 +12,7 @@ namespace EJournalWPF.Model
         public DateTime Date { get; set; }
         public string Subject { get; set; }
         public Student FromUser { get; set; }
-        public Status Status { get; set; }
+        public bool Readed { get; set; }
         public List<File> Files { get; set; }
         public bool hasFiles { get; set; }
 
