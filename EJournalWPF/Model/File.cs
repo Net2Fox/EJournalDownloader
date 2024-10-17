cfa110e97ec4768e31cf648f0fd80d9d60cd65c7
Net2Fox <trakounet.fox@gmail.com>
2024-10-17 10:03:57 +0300

    Добавлены конструкторы

--- EJournalWPF/Model/File.cs
+++ EJournalWPF/Model/File.cs
@@ -5,5 +5,12 @@
         public long ID { get; set; }
         public string Filename { get; set; }
         public string URL { get; set; }
+
+        public File(long id, string filename, string url)
+        {
+            ID = id;
+            Filename = filename;
+            URL = url;
+        }
     }
 }
--- EJournalWPF/Model/Mail.cs
+++ EJournalWPF/Model/Mail.cs
@@ -9,9 +9,30 @@ namespace EJournalWPF.Model
         public DateTime Date { get; set; }
         public string Subject { get; set; }
         public Student FromUser { get; set; }
-        public bool Readed { get; set; }
+        public Status Status { get; set; }
         public List<File> Files { get; set; }
         public bool hasFiles { get; set; }
 
+        public Mail(long id, DateTime date, string subject, Student fromUser, Status status, List<File> files)
+        {
+            this.ID = id;
+            this.Date = date;
+            this.Subject = subject;
+            this.FromUser = fromUser;
+            this.Status = status;
+            this.Files = files;
+            hasFiles = true;
+        }
+
+        public Mail(long id, DateTime date, string subject, Student fromUser, Status status)
+        {
+            this.ID = id;
+            this.Date = date;
+            this.Subject = subject;
+            this.FromUser = fromUser;
+            this.Status = status;
+            hasFiles = false;
+            Files = null;
+        }
     }
 }
