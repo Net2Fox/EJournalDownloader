d087df2abd8c1a5ba8511cfc9702f70ed1b97d5d
Net2Fox <trakounet.fox@gmail.com>
2024-10-18 11:35:39 +0300

    Добавлена фильтрация

--- EJournalWPF/Pages/MainPage.xaml.cs
+++ EJournalWPF/Pages/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using EJournalWPF.Model;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -12,6 +13,7 @@ namespace EJournalWPF.Pages
     /// </summary>
     public partial class MainPage : Page
     {
+        private bool isDataLoaded = false;
         public MainPage(List<CefSharp.Cookie> cefSharpCookies)
         {
             InitializeComponent();
@@ -25,7 +27,10 @@ namespace EJournalWPF.Pages
 
         private void LoadData(List<Mail> mails)
         {
-            Application.Current.Dispatcher.Invoke(() => EmailListBox.ItemsSource = mails);
+            Application.Current.Dispatcher.Invoke(() => {
+                EmailListBox.ItemsSource = mails;
+                isDataLoaded = true;
+            });
         }
 
         private void UpdateDownloadText(string message)
@@ -35,7 +40,7 @@ namespace EJournalWPF.Pages
 
         private void UpdateDownloadProgress(int value)
         {
-            
+
             Application.Current.Dispatcher.Invoke(() => DownloadBar.Value += value);
         }
 
@@ -45,6 +50,44 @@ namespace EJournalWPF.Pages
                 DownloadBar.Value = 0;
                 DownloadBar.Maximum = maximum;
             });
+        }
+
+        private void Filter()
+        {
+            List<Mail> filteredList = DataRepository.GetInstance().GetMails();
+
+            if (SearchTextBox.Text != String.Empty)
+            {
+                string text = SearchTextBox.Text.ToLower();
+                filteredList = filteredList.Where(m =>
+                m.FromUser.FirtsName.ToLower().Contains(text)
+                || m.FromUser.LastName.ToLower().Contains(text)
+                || m.FromUser.MiddleName.ToLower().Contains(text)
+                || m.Subject.ToLower().Contains(text)).ToList();
+            }
+
+            if (filteredList.Count != 0 && StatusComboBox.SelectedIndex != 0)
+            {
+                filteredList = filteredList.Where(m => ((int)m.Status) == StatusComboBox.SelectedIndex).ToList();
+            }
+
+            EmailListBox.ItemsSource = filteredList;
+        }
+
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (isDataLoaded == true)
+            {
+                Filter();
+            }
+        }
+
+        private void StatusComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (isDataLoaded == true)
+            {
+                Filter();
+            }
         }
     }
 }
